0a55902daa7fdbd51c2189818a698150526a9657
lovitesfay <lovitesfay@gmail.com>
2026-05-20 20:57:09 -0500

    updated the input and output of the user

--- src/main/java/com/pluralsight/loops/Program.java
+++ src/main/java/com/pluralsight/loops/Program.java
@@ -11,23 +11,22 @@ public class Program {
         List<Person> persons = new ArrayList<>();
 
         persons.add(new Person("lovi", "tesfay"));
-        persons.add(new Person("lovi", "tesfay"));
-        persons.add(new Person("lovi", "tesfay"));
-        persons.add(new Person("lovi", "tesfay"));
-        persons.add(new Person("lovi", "tesfay"));
-        persons.add(new Person("lovi", "tesfay"));
-        persons.add(new Person("lovi", "tesfay"));
-        persons.add(new Person("lovi", "tesfay"));
-        persons.add(new Person("lovi", "tesfay"));
-        persons.add(new Person("lovi", "tesfay"));
+        persons.add(new Person("biruk", "tafese"));
+        persons.add(new Person("justin", "king"));
+        persons.add(new Person("larry", "legend"));
+        persons.add(new Person("mark", "moten"));
+        persons.add(new Person("jackie", "chan"));
+        persons.add(new Person("steve", "harvey"));
+        persons.add(new Person("michael", "jordan"));
+        persons.add(new Person("christiano", "ronaldo"));
+        persons.add(new Person("messi", "buzy"));
 
         System.out.println("Search for a name (first or last): ");
+        String searchName = sc.nextLine();
 
         List<Person> matchedPeople = new ArrayList<Person>();
 
 
-
-            String searchName = "Lovi";
 
             for (Person person : persons) {
 
